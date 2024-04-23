9b25b06b5a990aedb4a182a8c4fe3eb2b8bedb78
YannickOclock <yannick.kuhn@oclock.io>
2024-04-23 20:35:01 +0200

    Ajout des 2 méthodes then + async/await

--- js/promise.js
+++ js/promise.js
@@ -11,7 +11,20 @@ const modulePromise = {
         return promise;
     },
 
-    init: function() {
+    methodAsyncAwait: async function() {
+        try {
+            const result1 = await modulePromise.promiseSetTimeOut(2000, "Faire mes courses");
+            console.log(result1);
+            const result2 = await modulePromise.promiseSetTimeOut(3000, "Faire la vaisselle");
+            console.log(result2);
+            const result3 = await modulePromise.promiseSetTimeOut(5000);
+            console.log(result3);
+        } catch(error) {
+            console.log(error);
+        }
+    },
+
+    methodThen: function() {
         // test setTimeOut
         modulePromise.promiseSetTimeOut(2000, "Faire mes courses").then((result) => {
             console.log(result);
@@ -22,5 +35,11 @@ const modulePromise = {
         }).catch((error) => {
             console.log(error);
         });
+    },
+
+    init: function() {
+        console.log('modulePromise.init');
+        modulePromise.methodThen();
+        modulePromise.methodAsyncAwait();
     }
 }
